847aa33946798f96a492ca1069a0822117eb3e42
Dami Adetula <dami.adetula@gmail.com>
2024-11-03 20:15:27 -0600

    initial commit/added sm icons

--- bagira124/src/components/HomeSection.jsx
+++ bagira124/src/components/HomeSection.jsx
@@ -29,101 +29,40 @@ const HomeSection = () => {
 
           <div className="hero-bottom">
             <div className="hero-bottom-left">
-              <div className="hero-call-wrap">
-                <div className="hero-call-bola"></div>
-
-                <div className="josefin-400-13">contact us today</div>
+              {/* Social Media Icons */}
+              <div className="social-icons">
+                <a href="https://www.instagram.com/theonlybagirayouknow/" target="_blank" rel="noopener noreferrer">
+                  <img src="images/instagramIcon.svg" alt="Instagram" className="icon-image" />
+                </a>
+                <a href="https://wa.me/+59996863862" target="_blank" rel="noopener noreferrer">
+                  <img src="/images/whatsappIcon.svg" alt="WhatsApp" className="icon-image" />
+                </a>
+                <a href="https://maps.app.goo.gl/THwn6N8VuZWE1TBH6" target="_blank" rel="noopener noreferrer">
+                  <img src="/images/locationIcon.svg" alt="Location" className="icon-image" />
+                </a>
               </div>
-
-              <div className="hero-left-left">
-                <div className="hero-contactos-wrap">
-                  <div
-                    data-w-id="0d6ae4b5-432f-a94f-2236-ad270b9bed79"
-                    className="small-links"
-                  >
-                    <div className="collection-numero w-dyn-list">
-                      <div
-                        role="list"
-                        className="collection-numero w-dyn-items"
-                      >
-                        <div
-                          role="listitem"
-                          className="collection-numero w-dyn-item"
-                        >
-                          <a
-                            href="tel:+5999 686 38 62"
-                            className="josefin-400-13"
-                          >
-                            +5999 686 38 62
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div
-                      data-w-id="09a4298e-c598-4700-e316-d7965799be14"
-                      className="nav-ball"
-                    ></div>
-                  </div>
-
-                  <div className="mobile-frase">
-                    <div className="josefin-400-10">
-                      Kura Hulanda Village, Curaçao
-                    </div>
-                  </div>
-
-                  <div className="small-links">
-                    <div className="collection-numero w-dyn-list">
-                      <div
-                        role="list"
-                        className="collection-numero w-dyn-items"
-                      >
-                        <div
-                          role="listitem"
-                          className="collection-numero w-dyn-item"
-                        >
-                          <a href="tel:" className="josefin-400-13">
-                            Message Me on whatsapp
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-
-                    <div className="nav-ball"></div>
-                  </div>
-
-                  <div className="mobile-frase">
-                    <div className="josefin-400-10"></div>
-                  </div>
+{/* 
+              <div className="hero-contactos-wrap">
+                <div className="josefin-400-13">CONTACT US TODAY</div>
+                <a href="tel:+59996863862" className="josefin-400-13">
+                  +5999 686 38 62
+                </a>
+                <div className="mobile-frase">
+                  <div className="josefin-400-10">Kura Hulanda Village, Curaçao</div>
                 </div>
-
-                <div className="hero-energia">
-                  <img
-                    src="/images/64380da380fc5a66db7f00d4_energia.svg"
-                    loading="lazy"
-                    alt=""
-                    className="image-energia"
-                  />
-                </div>
-              </div>
+              </div> */}
             </div>
 
             <div className="hero-scroll">
               <a
-                data-w-id="cd947534-154b-d008-a35f-bce9f4afa8aa"
                 href="#about"
                 className="big-cta about max-w-full h-full inline-block"
               >
                 <div className="big-cta-text z-10">
                   <div className="josefin-600-20">scroll</div>
                 </div>
-
                 <div className="big-cta-hover"></div>
-
-                <div
-                  data-w-id="65cef19a-0440-5f3f-adcc-8c93f997466a"
-                  className="big-cta-sticky"
-                ></div>
+                <div className="big-cta-sticky"></div>
               </a>
             </div>
           </div>
@@ -137,11 +76,7 @@ const HomeSection = () => {
         alt="BAGIRA"
         className="hero-img-background"
       />
-
-      <div
-        data-w-id="ecb77a20-c4eb-e3f3-474f-cce4b1522386"
-        className="hero-img-gradient"
-      ></div>
+      <div className="hero-img-gradient"></div>
     </div>
   );
 };
